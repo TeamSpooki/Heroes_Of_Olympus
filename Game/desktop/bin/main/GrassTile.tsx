064a3dbdd824f90df06241f0424ba8cea6362a69
Taran Bhogal <taranbhogal@live.co.uk>
2020-02-06 18:13:39 +0000

    Tazz First Commit

---
+++ Game/desktop/bin/main/GrassTile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="GrassTile" tilewidth="64" tileheight="64" tilecount="2048" columns="32">
+ <image source="GrassTile.png" width="2048" height="4096"/>
+</tileset>
